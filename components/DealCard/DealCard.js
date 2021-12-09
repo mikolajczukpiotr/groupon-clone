ff0318ca619849fec2aa672c5da47f6ef5fbb547
Piotr Mikołajczuk <piotrmikolajczuk@MacBook-Pro-Piotr.local>
2021-12-09 12:14:39 +0100

    deconstruction deal card to small components

--- components/DealCard/DealCard.js
+++ components/DealCard/DealCard.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { Badge, Box, Image } from "@chakra-ui/react";
 import { Icon, StarIcon } from "@chakra-ui/icons";
+import Type from "./Type";
 import Price from "./Price";
 import Rating from "./Rating";
 import Address from "./Address";
 
 const DealCard = () => {
-  const UpIcon = (props) => (
-    <Icon viewBox="0 0 16 16" {...props}>
-      <path
-        fill="rgba(102,80,215,1.0)"
-        d="M15.38 2.51l-6.02 1.5a.5.5 0 0 0-.23.85l1.37 1.37-2.52 2.52-2.33-2.33a.25.25 0 0 0-.36 0L.07 11.64c-.1.1-.1.26 0 .36L1.5 13.4c.05.05.11.08.18.08.06 0 .13-.03.18-.08L5.47 9.8l2.33 2.33c.1.1.26.1.36 0L12.27 8l1.37 1.37a.5.5 0 0 0 .84-.23l1.5-6.02a.5.5 0 0 0-.6-.6z"
-      />
-    </Icon>
-  );
-
   const property = {
     title: "ESC House",
     shortDescription: "Pokój Zagadki Sherlocka Holmesa",
@@ -51,13 +43,8 @@ const DealCard = () => {
         src={property.image["600"]}
         alt={property.image.alt}
       />
-
       <Box p="1">
-        <Box display="flex" alignItems="center">
-          <UpIcon className="mr-1" />
-          <Box textColor="#7561DB">Popularny</Box>
-        </Box>
-
+        <Type type={property.type} />
         <Box fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
           {property.title}
         </Box>
--- components/DealCard/Type.js
+++ components/DealCard/Type.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/icons";
 
-const Type = () => {
-  return <div></div>;
+const Type = ({ type }) => {
+  const UpIcon = (props) => (
+    <Icon viewBox="0 0 16 16" {...props}>
+      <path
+        fill="rgba(102,80,215,1.0)"
+        d="M15.38 2.51l-6.02 1.5a.5.5 0 0 0-.23.85l1.37 1.37-2.52 2.52-2.33-2.33a.25.25 0 0 0-.36 0L.07 11.64c-.1.1-.1.26 0 .36L1.5 13.4c.05.05.11.08.18.08.06 0 .13-.03.18-.08L5.47 9.8l2.33 2.33c.1.1.26.1.36 0L12.27 8l1.37 1.37a.5.5 0 0 0 .84-.23l1.5-6.02a.5.5 0 0 0-.6-.6z"
+      />
+    </Icon>
+  );
+  return (
+    <Box display="flex" alignItems="center">
+      {type == "best" ? <UpIcon className="mr-1" /> : null}
+      {type == "best" ? <Box textColor="#7561DB">Popularny</Box> : null}
+    </Box>
+  );
 };
 
 export default Type;
