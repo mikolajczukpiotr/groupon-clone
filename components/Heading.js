c16193ecf156be9e57f942f62072630a8cf33c36
Piotr Mikołajczuk <piotrmikolajczuk@MacBook-Pro-Piotr.local>
2021-12-14 23:11:55 +0100

    added function first and second word is bold in heading

--- components/Heading.js
+++ components/Heading.js
@@ -30,7 +30,18 @@ const Heading = () => {
         <Progress size="xs" isIndeterminate colorScheme="green" />
       </>
     );
-  return <h1 className="flex p-5 w-2/3 m-auto text-2xl">{data[0].value}</h1>;
+  const headingText = data[0].value;
+  const words = headingText.split(" ");
+  const [first, second, ...rest] = words;
+  return (
+    <h1 className="flex p-5 w-2/3 m-auto text-2xl">
+      <span className="font-bold">
+        {" "}
+        {first} {second}
+      </span>
+      {rest.join(" ")}
+    </h1>
+  );
 };
 
 export default Heading;
