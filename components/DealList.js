615eb9f3ee69177e14f7c2b1d2dae91be9002855
Piotr Mikołajczuk <piotrmikolajczuk@MacBook-Pro-Piotr.local>
2021-12-14 21:57:13 +0100

    add error text and progress in loading alert

--- components/DealList.js
+++ components/DealList.js
@@ -1,14 +1,36 @@
 import React from "react";
 import DealCard from "./DealCard/DealCard";
 import useSWR from "swr";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Progress,
+} from "@chakra-ui/react";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const DealList = () => {
   const { data, error } = useSWR("/api/deals", fetcher);
-  console.log(data);
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  if (error)
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle mr={2}>Error!</AlertTitle>
+        <AlertDescription>
+          system nie działa w prawidłowy sposób, skontaktuj sie z
+          administratorem strony.
+        </AlertDescription>
+      </Alert>
+    );
+  if (!data)
+    return (
+      <>
+        <h1> LOADING...</h1>
+        <Progress size="xs" isIndeterminate colorScheme="green" />
+      </>
+    );
   return (
     <div className="flex p-auto w-2/3 m-auto">
       <DealCard property={data[0]} />
--- components/Heading.js
+++ components/Heading.js
@@ -1,13 +1,35 @@
 import React from "react";
 import useSWR from "swr";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Progress,
+} from "@chakra-ui/react";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Heading = () => {
   const { data, error } = useSWR("/api/site", fetcher);
-  console.log(data);
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  if (error)
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle mr={2}>Error!</AlertTitle>
+        <AlertDescription>
+          system nie działa w prawidłowy sposób, skontaktuj sie z
+          administratorem strony.
+        </AlertDescription>
+      </Alert>
+    );
+  if (!data)
+    return (
+      <>
+        <h1> LOADING...</h1>
+        <Progress size="xs" isIndeterminate colorScheme="green" />
+      </>
+    );
   return <h1 className="flex p-5 w-2/3 m-auto text-2xl">{data[0].value}</h1>;
 };
 
