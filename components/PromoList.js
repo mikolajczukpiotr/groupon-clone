f899d7672e013ee13630c17f71b4c2ef3bd891e8
Piotr Mikołajczuk <piotrmikolajczuk@MacBook-Pro-Piotr.local>
2021-12-21 12:56:43 +0100

    create promocard and promolist

---
+++ components/PromoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PromoCard from "./PromoCard";
+
+const PromoList = () => {
+  return (
+    <div className="flex p-auto w-full m-auto">
+      <PromoCard />
+      <PromoCard />
+    </div>
+  );
+};
+
+export default PromoList;
