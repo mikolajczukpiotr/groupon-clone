6806995ea5d694d99956a894de813d5ff3e6b01c
Piotr Mikołajczuk <piotrmikolajczuk@MacBook-Pro-Piotr.local>
2022-02-25 00:00:24 +0100

    add function hovering over icon change color

--- components/DealPage/ShareSection.js
+++ components/DealPage/ShareSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FacebookShareIcon,
   PinterestShareIcon,
@@ -6,18 +6,57 @@ import {
 } from "../Icons";
 
 const ShareSection = () => {
+  const [hoverFirstIcon, setHoverFirstIcon] = useState(false);
+  const [hoverSecondIcon, setHoverSecondIcon] = useState(false);
+  const [hoverThirdIcon, setHoverThirdIcon] = useState(false);
   return (
     <div className="mt-4">
       <div className="font-bold text-sm">UDOSTĘPNIJ OFERTĘ</div>
       <div className="mt-1 flex">
-        <div className="">
-          <FacebookShareIcon h={7} w={7} />
+        <div
+          onMouseEnter={() => {
+            setHoverFirstIcon(true);
+          }}
+          onMouseLeave={() => {
+            setHoverFirstIcon(false);
+          }}
+        >
+          <FacebookShareIcon
+            h={7}
+            w={7}
+            marginLeft={4}
+            color={hoverFirstIcon ? "#0070cc" : "#A5A8AB"}
+          />
         </div>
-        <div>
-          <PinterestShareIcon h={7} w={7} marginLeft={4} />
+        <div
+          onMouseEnter={() => {
+            setHoverSecondIcon(true);
+          }}
+          onMouseLeave={() => {
+            setHoverSecondIcon(false);
+          }}
+        >
+          <PinterestShareIcon
+            h={7}
+            w={7}
+            marginLeft={4}
+            color={hoverSecondIcon ? "#e00038" : "#A5A8AB"}
+          />
         </div>
-        <div>
-          <TwitterShareIcon h={7} w={7} marginLeft={4} />
+        <div
+          onMouseEnter={() => {
+            setHoverThirdIcon(true);
+          }}
+          onMouseLeave={() => {
+            setHoverThirdIcon(false);
+          }}
+        >
+          <TwitterShareIcon
+            h={7}
+            w={7}
+            marginLeft={4}
+            color={hoverThirdIcon ? "#55acee" : "#A5A8AB"}
+          />
         </div>
       </div>
     </div>
