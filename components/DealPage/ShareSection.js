9d206bf32a8b5abd245c09f4ee2f6c080f22dd07
Piotr Mikołajczuk <piotrmikolajczuk@MacBook-Pro-Piotr.local>
2022-03-01 22:43:04 +0100

    change icons to mapped from array

--- components/DealPage/ShareSection.js
+++ components/DealPage/ShareSection.js
@@ -4,49 +4,32 @@ import {
   PinterestShareIcon,
   TwitterShareIcon,
 } from "../Icons";
+import IconsShareSections from "./IconsShareSections";
+
+const icons = [
+  {
+    type: "facebook",
+    component: FacebookShareIcon,
+    selectedColor: "#0070cc",
+  },
+  {
+    type: "pinterest",
+    component: PinterestShareIcon,
+    selectedColor: "#e00038",
+  },
+  {
+    type: "twitter",
+    component: TwitterShareIcon,
+    selectedColor: "#55acee",
+  },
+];
 
 const ShareSection = () => {
-  const [hoverIcon, setHoverIcon] = useState("");
   return (
     <div className="mt-4">
       <div className="font-bold text-sm">UDOSTĘPNIJ OFERTĘ</div>
       <div className="mt-1 flex">
-        <div
-          onMouseEnter={() => {
-            setHoverIcon("firstIcon");
-          }}
-          onMouseLeave={() => {
-            setHoverIcon(null);
-          }}
-        >
-          <FacebookShareIcon
-            color={hoverIcon === "firstIcon" ? "#0070cc" : "#A5A8AB"}
-          />
-        </div>
-        <div
-          onMouseEnter={() => {
-            setHoverIcon("secondIcon");
-          }}
-          onMouseLeave={() => {
-            setHoverIcon(null);
-          }}
-        >
-          <PinterestShareIcon
-            color={hoverIcon === "secondIcon" ? "#e00038" : "#A5A8AB"}
-          />
-        </div>
-        <div
-          onMouseEnter={() => {
-            setHoverIcon("thirdIcon");
-          }}
-          onMouseLeave={() => {
-            setHoverIcon(null);
-          }}
-        >
-          <TwitterShareIcon
-            color={hoverIcon === "thirdIcon" ? "#55acee" : "#A5A8AB"}
-          />
-        </div>
+        <IconsShareSections icons={icons} />
       </div>
     </div>
   );
